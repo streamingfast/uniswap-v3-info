f370bef94609c1bdc53a0f74ef37f801dd701bc9
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-05-25 10:30:55 -0400

    update deployment hash

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -22,7 +22,7 @@ export const blockClient = new ApolloClient({
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://127.0.0.1:8000/subgraphs/id/QmdHM9Mb93c3CVUJtxBcGHycaZhmWBmb2JuGWpbcLZzKmJ',
+    uri: 'http://127.0.0.1:8000/subgraphs/id/QmQJovmQLigEwkMWGjMT8GbeS2gjDytqWCGL58BEhLu9Ag',
   }),
   cache: new InMemoryCache({
     typePolicies: {
